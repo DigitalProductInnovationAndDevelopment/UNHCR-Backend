8922c8326ee6b2f2d4cb69b1d47c2ccb5bdfecbe
Betul Karagoz <betulkaraagoz@gmail.com>
2024-08-11 11:40:54 +0200

    change the user endpoint for get user

--- EndUserManagement/views/user/usersController.py
+++ EndUserManagement/views/user/usersController.py
@@ -22,24 +22,37 @@ translationService = TranslationService()
 # Create your views here.
 @api_view(["GET", "POST"])
 def usersController(request, **kwargs):
-
     if request.method == "GET":
-        """
-        Lists all users.
-        @Endpoint: /users
-        """
-        try:
-            # The users can only be listed via the admin user list endpoint.
-            return Response(
+    """
+    Gets a user. It is designed for users to get info about their own accounts.
+    @Endpoint: /users
+    """
+
+    user = kwargs["loggedUser"]
+    # The case for end user tring to operate on another user except himself/herself
+    # Use admin endpoints for operating on other users
+    if not user:
+        return Response(
                 {"success": False, "message": translationService.translate("HTTP.not.authorized")},
                 status=status.HTTP_401_UNAUTHORIZED,
             )
-        except Exception as e:
-            logger.error(traceback.format_exc())
-            return Response(
-                {"success": False, "message": translationService.translate('general.exception.message')},
-                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            )
+    try: 
+        responseSerializer = UserGetResponseSerializer(user)
+        return Response(
+            {"success": True, "data": responseSerializer.data},
+            status=status.HTTP_200_OK,
+        )
+    except User.DoesNotExist:
+        return Response(
+            {"success": False, "message": translationService.translate('user.not.exist')},
+            status=status.HTTP_404_NOT_FOUND,
+        )
+    except Exception as e:
+        logger.error(traceback.format_exc())
+        return Response(
+            {"success": False, "message": translationService.translate('general.exception.message')},
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
 
     elif request.method == "POST":
         """
