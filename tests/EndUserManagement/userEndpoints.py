888ff53172994fa2a683fd29df9af09d4477958f
fireflowmert <sahin@fireflow.de>
2024-08-13 14:25:10 +0200

    test get user

---
+++ tests/EndUserManagement/userEndpoints.py
@@ -0,0 +1,72 @@
+import requests
+
+from django.test import TestCase
+from EndUserManagement.models import Case
+from EndUserManagement.dummyData import getDummyCases, getDummyUsers
+
+
+class CasesTestCase(TestCase):
+    def setUp(self):
+        # The host for the docker 
+        self.baseServerUrl = "http://localhost:8000/api"
+        self.dummyUserEmail = "eduard@gmail.com"
+        self.dummyUserPwd = "1234567"
+        payload = {
+            "EmailAddress": self.dummyUserEmail,
+            "Password": self.dummyUserPwd
+        }
+        loginUrl = self.baseServerUrl + "/login"
+        # Send a POST request to the login endpoint
+        response = requests.post(loginUrl, json=payload)
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # Parse the JSON response
+            responseData = response.json()   
+            # Extract the accessToken from the response data
+            accessToken = responseData.get('data', {}).get('access_token')
+            if accessToken:
+                self.dummyUserAccessToken = accessToken
+            else:
+                raise Exception("No access token in the login response. Check out the test set up method.")
+        else:
+            raise Exception("Login request failed. Check out the test set up method.")
+
+    def test_get_user(self):
+        getUserUrl = self.baseServerUrl + "/users"
+        getUserHeaders = {
+            'Authorization': f'Bearer {self.dummyUserAccessToken}'
+        }
+        response = requests.get(getUserUrl, headers = getUserHeaders)
+        responseData = None
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # Parse the JSON response
+            responseData = response.json()      
+        else:
+            raise Exception("Get user request failed.")
+        # Check if response is not an empty list
+        userFetched = responseData['data']
+        self.assertNotEqual(userFetched, [])
+        self.assertEqual(len(userFetched), 1)
+        dummyUsers = getDummyUsers()
+        
+     
+
+        name_to_find = "Eduard"
+        userExpected = [user for user in dummyUsers if user["Name"] == name_to_find]
+        userObserved = userFetched
+    
+        # Comparing the expected and observed emails of the cases
+        self.assertIsNotNone(userExpected,userObserved)
+        self.assertEqual(userExpected["EmailAddress"], userObserved["EmailAddress"])
+
+    def test_case_create(self):
+        """Animals that can speak are correctly identified"""
+        print("test_case_create")
+
+    def test_case_get(self):
+        """Animals that can speak are correctly identified"""
+        print("test_case_get")
+    
+    # NO CASE DELETE AND UPDATE FOR THE END USER
