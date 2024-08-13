c9d3af61af9505af8605221198de6d635e5eb155
Betul Karagoz <betulkaraagoz@gmail.com>
2024-08-13 11:15:28 +0200

    add a test field to test pipeline

--- EndUserManagement/models/MessageMedia.py
+++ EndUserManagement/models/MessageMedia.py
@@ -11,6 +11,7 @@ class MessageMedia(TimestampAbstractModel):
     Message = models.ForeignKey(Message, db_column = 'Message', on_delete = models.CASCADE)
     MediaType = models.TextField()
     MediaName = models.TextField()
+    TestField = models.TextField()
 
     class Meta:
         db_table = 'MessageMedia'
